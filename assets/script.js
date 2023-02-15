6042a58c3055d7a3ddeed49d83fd72c36dcb03d1
James A Cone <jacone626@gmail.com>
2023-02-15 14:36:44 -0500

    added a timer to the js file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,23 @@
-//JavaScript file
+//var generateBtn = document.querySelector(".start-button")
+var timerEl = document.querySelector(".timer-count")
+
+//generateBtn.addEventListener("click", startGame)
+
+//function startGame() {
+//    countdown();
+//}
+
+function countdown() {
+
+    var timeLeft = 75;
+
+    var timeInterval = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent = timeLeft;
+      if (timeLeft === 0) {
+        clearInterval(timeInterval);
+      }
+    },1000);
+  }
+
+  countdown();
