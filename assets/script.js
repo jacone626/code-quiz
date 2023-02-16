001aad4dbab8b8c5cd81b483bbeb6b9b13c078f8
James A Cone <jacone626@gmail.com>
2023-02-16 16:08:40 -0500

    added some CSS styling to the questions and continued to add function in js

--- assets/script.js
+++ assets/script.js
@@ -25,52 +25,164 @@ function countdown() {
   var Questions = [{
     id: 0,
     q: "Commonly used data types DO NOT include:",
-    a: [{ text: "strings", isCorrect: false },
-        { text: "booleans", isCorrect: false },
-        { text: "alerts", isCorrect: true },
-        { text: "numbers", isCorrect: false }
+    a: [{ text: "1. strings", isCorrect: false },
+        { text: "2. booleans", isCorrect: false },
+        { text: "3. alerts", isCorrect: true },
+        { text: "4. numbers", isCorrect: false }
     ]
 
 },
 {
     id: 1,
     q: "The condition in an if/else stateent is enclosed within:",
-    a: [{ text: "quotes", isCorrect: false },
-        { text: "curly brackets", isCorrect: false },
-        { text: "parentheses", isCorrect: true },
-        { text: "square brackets", isCorrect: false}
+    a: [{ text: "1. quotes", isCorrect: false },
+        { text: "2. curly brackets", isCorrect: false },
+        { text: "3. parentheses", isCorrect: true },
+        { text: "4. square brackets", isCorrect: false}
     ]
 
 },
 {
     id: 2,
     q: "Arrays in JavaScript can be used to store:",
-    a: [{ text: "numbers and strings", isCorrect: false },
-        { text: "other arrays", isCorrect: false },
-        { text: "booleans", isCorrect: false },
-        { text: "all of the above", isCorrect: true }
+    a: [{ text: "1. numbers and strings", isCorrect: false },
+        { text: "2. other arrays", isCorrect: false },
+        { text: "3. booleans", isCorrect: false },
+        { text: "4. all of the above", isCorrect: true }
     ]
 
 },
 {
     id: 3,
     q: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    a: [{ text: "JavaScript", isCorrect: false, isSelected: false },
-        { text: "terminal/bash", isCorrect: false },
-        { text: "for loops", isCorrect: false },
-        { text: "console.log", isCorrect: true }
+    a: [{ text: "1. JavaScript", isCorrect: false, isSelected: false },
+        { text: "2. terminal/bash", isCorrect: false },
+        { text: "3. for loops", isCorrect: false },
+        { text: "4. console.log", isCorrect: true }
     ]
 
 },
 {
     id: 4,
     q: "String values must be enclosed within     when being assigned to variables.",
-    a: [{ text: "commas", isCorrect: false },
-        { text: "curly brackets", isCorrect: false },
-        { text: "quotes", isCorrect: true },
-        { text: "parentheses", isCorrect: false }
+    a: [{ text: "1. commas", isCorrect: false },
+        { text: "2. curly brackets", isCorrect: false },
+        { text: "3. quotes", isCorrect: true },
+        { text: "4. parentheses", isCorrect: false }
     ]
 
 }
 
 ]
+
+// Set start
+var start = true;
+  
+// Iterate
+function iterate(id) {
+  
+    // Getting the result display section
+    var result = document.getElementsByClassName("result");
+    result[0].innerText = "";
+  
+    // Getting the question
+    var question = document.getElementById("question");
+  
+  
+    // Setting the question text
+    question.innerText = Questions[id].q;
+  
+    // Getting the options
+    var op1 = document.getElementById('op1');
+    var op2 = document.getElementById('op2');
+    var op3 = document.getElementById('op3');
+    var op4 = document.getElementById('op4');
+  
+  
+    // Providing option text 
+    op1.innerText = Questions[id].a[0].text;
+    op2.innerText = Questions[id].a[1].text;
+    op3.innerText = Questions[id].a[2].text;
+    op4.innerText = Questions[id].a[3].text;
+  
+    // Providing the true or false value to the options
+    op1.value = Questions[id].a[0].isCorrect;
+    op2.value = Questions[id].a[1].isCorrect;
+    op3.value = Questions[id].a[2].isCorrect;
+    op4.value = Questions[id].a[3].isCorrect;
+  
+
+
+// Show selection for op1
+op1.addEventListener("click", () => {
+    selected = op1.value;
+})
+
+// Show selection for op2
+op2.addEventListener("click", () => {
+    selected = op2.value;
+})
+
+// Show selection for op3
+op3.addEventListener("click", () => {
+    selected = op3.value;
+})
+
+// Show selection for op4
+op4.addEventListener("click", () => {
+    selected = op4.value;
+})
+
+// Moving to the next question after selecting an option
+
+var optionsButton = document.getElementsByClassName("option")
+
+optionsButton.addEventListener("click", () => {
+    // Get the user's selected answer
+    var userChoice = document.querySelector("option");
+    if (userChoice) {
+      // Check if the user's answer is correct and increment the score
+      var currentQuizQuestion = quizQuestions[currentQuestion];
+      if (userChoice.value === currentQuizQuestion.correctAnswer) {
+        result[0].innerHTML = "Correct!";
+      }
+  
+      // Move on to the next question
+      currentQuestion++;
+      if (currentQuestion < quizQuestions.length) {
+        showQuestion();
+      } else {
+        // If there are no more questions, show the score 
+      }
+    }
+  });
+
+
+var result = document.getElementsByClassName("result");
+// Evaluate method
+result[0].addEventListener("click", () => {
+    if (selected == "true") {
+        result[0].innerHTML = "Correct!";
+    } else {
+        result[0].innerHTML = "Wrong!";
+    }
+})
+}
+
+if (start) {
+iterate("0");
+}
+
+// Next button and method
+var next = document.getElementsByClassName('next')[0];
+var id = 0;
+
+next.addEventListener("click", () => {
+start = false;
+if (id < 2) {
+    id++;
+    iterate(id);
+    console.log(id);
+}
+
+})
